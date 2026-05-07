7634495225b9f7dca5f363acad66b1b3de5279ae
Javier González Martín <javier.gonzalez@omniaccess.com>
2026-05-07 09:37:05 +0200

    fix: set AP_ENV correctly

--- mobile/constants/config.ts
+++ mobile/constants/config.ts
@@ -2,7 +2,7 @@ const DEV_API_URL = "http://localhost:8000/api/v1";
 const PROD_API_URL = "https://usoluz-backend.onrender.com/api/v1";
 
 export const Config = {
-  apiUrl: process.env.APP_ENV === "production" ? PROD_API_URL : DEV_API_URL,
+  apiUrl: process.env.EXPO_PUBLIC_APP_ENV === "production" ? PROD_API_URL : DEV_API_URL,
   defaultProvider: "ree_pvpc",
   refetchInterval: 1000 * 60 * 5, // 5 minutes
 } as const;
